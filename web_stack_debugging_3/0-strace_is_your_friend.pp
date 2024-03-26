5400acc9e7f0eb8c6714d5edd08ef0d92fb54287
Kiteka2 <k.iteka@alustudent.com>
2024-03-26 11:09:19 +0000

    task one

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+# Fix Appache 500, automate with puppet
+exec { 'fixing typo...':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}# Fix Appache 500, automate with puppet
+exec { 'fixing typo...':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
